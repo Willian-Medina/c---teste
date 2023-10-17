dfb60a20c26023035659bf08383f30e77c3683a0
willian medina <willian-pablomedina@hotmail.com>
2023-10-16 23:06:52 -0300

    alterado para pegar nome

--- Program.cs
+++ Program.cs
@@ -1,3 +1,22 @@
-﻿
+﻿namespace primaryProgram;
+
+class chamar_nome {
+
+    //sempre colocar o static void main(){}
+static void Main(){
+
+    //void porque não irá retornar nada
+
+// deve sempre usar aspas duplas
+
+if(Console.ReadLine() == "willian"){
+    Console.WriteLine($"seja bem vindo meu querido");
+} else {
+    Console.WriteLine ("infelizmente você não está autorizado a entrar");
+}
+
+    }}
+
+
 
 
